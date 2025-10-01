fef971b7f534e000b408e61fb92f581084f72e5c
IncogNeato <justincihi@gmail.com>
2025-10-01 03:14:56 -0400

    fix: Replace unicode with str

---
+++ example.py
@@ -0,0 +1,9 @@
+# Example file demonstrating the unicode to str conversion
+
+# Before:
+# some_var = unicode(some_other_var)
+
+# After:
+some_var = str(some_other_var)
+
+# Additional code can follow...
